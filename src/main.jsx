4210d015dbf016c4634e6b6981657cd45e6a5e7e
FatimaDuret <f.duret.g@gmail.com>
2024-05-24 08:44:14 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './styles/app.scss';
 import App from './components/App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+      <App />
+    
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
